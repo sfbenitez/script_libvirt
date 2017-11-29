be757505b58e8654845344bf4acd54aa22db0f3e
sfbenitez <sergioferretebenitez@gmail.com>
2017-11-29 13:40:31 +0100

    Last release

--- check_mem.sh
+++ check_mem.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-PERCENT_ALLOWED=50
+PERCENT_ALLOWED=90
 
 test_memory(){
     MAXMEM=$(free | grep "Mem"|awk -F' ' '{print $2}')
     USEDMEM=$(expr `free | grep "Mem"|awk -F' ' '{print $3}'` \* 100)
-    PERCENTAGE=$(expr $USEDMEN / $MAXMEM)
+    PERCENTAGE=$(expr $USEDMEM / $MAXMEM)
     # Si el porcentaje usado es mayor que porcentaje_alerta return 1
-    [[ $PERCENTAGE>$PERCENT_ALLOWED ]] && return 1 || return 0
+    [[ $PERCENTAGE>$PERCENT_ALLOWED ]] && return 0 || return 1
 }
 if test_memory;
 then
   umount /srv/www
-  echo "HELP" | ssh ferrete@10.10.10.1 "cat > /home/ferrete/$HOSTNAME"
+  echo "HELP" | ssh root@10.10.10.1 "cat > /root/$HOSTNAME"
 fi
--- host.sh
+++ host.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if grep -Fxq "HELP" /home/ferrete/MV1
+if grep -Fxq "HELP" /root/mv1;
 then
   #deattach
-  virsh detach-disk MV1 vdb --live --config
+  virsh detach-disk MV1 vdb --live
   #aumentar tamaño
   lvresize -L 2G /dev/volumenes1/apache
   #montar local
@@ -12,12 +12,17 @@ then
   #desmontar local
   umount /mnt/
   #attach mv2
-  virsh attach-disk MV2 --source /dev/vgferrete/apache --target vdb --persistent
+  virsh attach-disk MV2 --source /dev/volumenes1/apache --target vdb --live
   #ejecutar montar en mv2
-  ssh root@10.10.10.3 "/root/MV2"
+  ssh root@10.10.10.3 "/root/mount_disk.sh"
+  rm /root/mv1
 fi
 
-if grep -Fxq "HELP" /home/ferrete/MV2
+if grep -Fxq "HELP" /root/mv2;
 then
   # resize Mem
   virsh setmem MV2 2G --live
+  rm /root/mv2
+fi
+# stress memory MV1 stress -m 1 --vm-bytes 420M --vm-keep -t 70s
+# stress memory MV2 stress -m 1 --vm-bytes 840M --vm-keep -t 70s
