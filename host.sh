1b11371376f07a807e0ac0e90f068652db25112e
Ferrete <sergioferretebenitez@gmail.com>
2017-11-29 21:39:23 +0100

    Adding all comments

--- host.sh
+++ host.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
+# Comprobar si necesita ayuda MV1
 if grep -Fxq "HELP" /root/mv1;
 then
-  #deattach
+  # Quitar disco a MV1 en caliente
   virsh detach-disk MV1 vdb --live
-  #aumentar tamaño
+  # Redimensionar el volumen de la aplicación
   lvresize -L 2G /dev/volumenes1/apache
-  #montar local
+  # Motar volumen en local para poder redimensionar el sistema de ficheros
   mount /dev/volumenes1/apache /mnt/
-  #redimensionar-fs
+  # Redimesionar el sistema de ficheros
   xfs_growfs /dev/volumenes1/apache
-  #desmontar local
+  # Desmontar el volumen
   umount /mnt/
-  #attach mv2
+  # Añadir el volumen a MV2 en caliente
   virsh attach-disk MV2 --source /dev/volumenes1/apache --target vdb --live
-  # cambia regla iptable
+  # Eliminar regla iptables redirigida hacia MV1 y añadir una regla dirigida hacia MV2
   iptables -t nat -D PREROUTING -i br0 -p tcp --dport 80 -j DNAT --to 10.10.10.2:80
   iptables -t nat -A PREROUTING -i br0 -p tcp --dport 80 -j DNAT --to 10.10.10.3:80
-  #ejecutar montar en mv2
+  # Montar el volumen en MV2
   ssh root@10.10.10.3 "/root/mount_disk.sh"
+  # Elimiar el fichero de ayuda
   rm /root/mv1
 fi
-
+# Comprobar si necesita ayuda MV2
 if grep -Fxq "HELP" /root/mv2;
 then
-  # resize Mem
+  # Redimensionar la memoria actual de MV2
   virsh setmem MV2 2G --live
+  # Eliminar el fichero de ayuda
   rm /root/mv2
 fi
-# stress memory MV1 stress -m 1 --vm-bytes 420M --vm-keep -t 70s
-# stress memory MV2 stress -m 1 --vm-bytes 840M --vm-keep -t 70s
+# stress memory MV1: stress -m 1 --vm-bytes 420M --vm-keep -t 70s
+# stress memory MV2: stress -m 1 --vm-bytes 840M --vm-keep -t 70s
